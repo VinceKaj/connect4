4eb55b820546a5a61bf581d846f1f2860f84afea
VinceKaj <35348750+VinceKaj@users.noreply.github.com>
2022-06-06 14:44:59 +0300

    Updated ClickButton

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class App(tk.Tk):
         # on top of each other, then the one we want visible
         # will be raised above the others
         container = tk.Frame(self)                              # initialize the main frame of the window
-        container.pack(side="top", fill="both", expand=True)
+        container.grid()
         container.grid_rowconfigure(0, weight=1)                # configure rows
         container.grid_columnconfigure(0, weight=1)             # configure columns
 
@@ -67,8 +67,10 @@ class PlayerSelect(tk.Frame):
         tk.Radiobutton(self, text="Player vs Computer", variable=isPVP, value=0).grid(column=1, row=5)
 
         tk.Label(self, text="Player names", font=HEADER2).grid(column=3, row=3)
+        name1 = tk.Entry().grid(column=3, row=4)
+        name2 = tk.Entry().grid(column=3, row=5)
 
-        tk.Button(self, text="Play!", command=lambda: controller.show_frame("GameWindow")).grid(column=2, row=4)
+        tk.Button(self, text="Play!", command=lambda: controller.show_frame("GameWindow")).grid(column=2, row=6)
 
 
 class GameWindow(tk.Frame):
@@ -77,12 +79,19 @@ class GameWindow(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        """ Initialize players """
+        
+        players.append(Player(PlayerSelect.name1.get(), 0, "#0000ff"))
+        players.append(Player(PlayerSelect.name2.get(), 0, "#ff0000"))
+
+        """ Initialize board """
+
         for y in range(6):    # loop through eachh row
           row = []            # initalize row array
           invisRow = []
 
           for x in range(7):  # loop through each column
-            row.append(tk.Button(self, text=f"{x},{y}").grid(column=x, row=y)) # add a button in each slot
+            row.append(tk.Button(self, text=f"{x},{y}", command=lambda: ClickButton(x)).grid(column=x, row=y)) # add a button in each slot
             invisRow.append(' ')
 
           buttonBoard.append(row) # add the row of buttons to the 2D button board array
@@ -97,19 +106,40 @@ class WinWindow(tk.Frame):
         label = tk.Label(self, text="This page is empty.", font=HEADER1).grid(column=1, row=1)
         button = tk.Button(self, text="Player Select", command=lambda: controller.show_frame("PlayerSelect"), font=HEADER1).grid(column=1, row=1)
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
-
 def FindFreeTile(x):
+
     global board
+
     for b in range(6,0,-1):
         if board[x,b] == "":
             return b
+
     return -1
 
 def UpdateBoard(x,y):
     global board 
-    if player1turn == True:
-        board[x,y] = 
 
+    if player1turn:
+        board[x,y] = "1"
+    else:
+        board[x,y] = "2"
+
+def isWin(x,y):
+    
+    return False
+
+def ClickButton(x):
+
+    y = FindFreeTile(x)
+    UpdateBoard(x,y)
+
+    if (isWin(x,y)):
+        WinScreen()
+    else:
+        ChangeTurn()
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
